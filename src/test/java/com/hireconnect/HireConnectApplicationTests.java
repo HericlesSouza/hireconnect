e64224c1f84a96086bb370c254c70e48c90444bb
Hericles Souza <hericles_003@hotmail.com>
2024-12-11 17:35:32 -0300

    first commit

---
+++ src/test/java/com/hireconnect/HireConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hireconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
