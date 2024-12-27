5cddfb5e19a105b2b8581dd67de5adb5aa7430ea
Hericles Souza <hericles_003@hotmail.com>
2024-12-26 21:31:49 -0300

    feat(DTO): create user's dto

---
+++ src/main/java/com/hireconnect/presentation/dto/user/UserCreateDTO.java
@@ -0,0 +1,38 @@
+package com.hireconnect.presentation.dto.user;
+
+import com.hireconnect.core.annotation.optionalNotBlank.OptionalNotBlank;
+import com.hireconnect.core.annotation.validEnum.ValidEnum;
+import com.hireconnect.core.entity.TypeUser;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCreateDTO {
+    @Size(max = 50, message = "the size must be less than 50 characters.")
+    @NotBlank(message = "name is required.")
+    private String name;
+
+    @Email(message = "must be an email.")
+    @NotBlank(message = "email is required.")
+    @Size(max = 50, message = "the size must be less than 50 characters.")
+    private String email;
+
+    @NotBlank(message = "password is required.")
+    private String password;
+
+    @Size(max = 2000, message = "the size must be less than 2000 characters.")
+    @OptionalNotBlank(message = "imgUrl must not be blank.")
+    private String imgUrl;
+
+    @NotNull(message = "typeUser is required.")
+    @ValidEnum(enumClass = TypeUser.class, message = "Invalid value for typeUser. Accepted values are: FREELANCER, ADMIN, COMPANY.")
+    private String typeUser;
+}
---
+++ src/main/java/com/hireconnect/presentation/dto/user/UserDTO.java
@@ -0,0 +1,22 @@
+package com.hireconnect.presentation.dto.user;
+
+import com.hireconnect.core.entity.TypeUser;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private UUID id;
+    private String name;
+    private String email;
+    private String imgUrl;
+    private TypeUser typeUser;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
