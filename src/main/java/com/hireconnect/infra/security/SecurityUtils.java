d6deb6968f8c89e9bf3d8ba0143fe11db4bbe71f
Hericles Souza <hericles_003@hotmail.com>
2025-01-06 13:27:34 -0300

    refactor(SecurityUtils): update to fetch authenticated user from database

--- src/main/java/com/hireconnect/infra/security/SecurityUtils.java
+++ src/main/java/com/hireconnect/infra/security/SecurityUtils.java
@@ -1,20 +1,28 @@
 package com.hireconnect.infra.security;
 
 import com.hireconnect.core.entity.User;
+import com.hireconnect.core.exception.ResourceNotFoundException;
+import com.hireconnect.core.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class SecurityUtils {
-    public static User getAuthenticatedUser() {
+    private final UserRepository userRepository;
+
+    public User getAuthenticatedUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !(authentication.getPrincipal() instanceof User)) {
-            throw new IllegalStateException("Usuário não autenticado ou inválido");
+
+        if (authentication == null || !(authentication.getPrincipal() instanceof UUID userId)) {
+            throw new IllegalStateException("User is not authenticated or has invalid credentials");
         }
-        return (User) authentication.getPrincipal();
+
+        return this.userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User with ID " + userId + " not found"));
     }
 }
-
