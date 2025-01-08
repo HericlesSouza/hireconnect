d5317193cf6a880629787b3d73fd5fc4a6ea564d
Hericles Souza <hericles_003@hotmail.com>
2025-01-08 10:17:39 -0300

    feat(Department): create entity and repository

--- src/main/java/com/hireconnect/core/entity/Department.java
+++ src/main/java/com/hireconnect/core/entity/Department.java
@@ -1,2 +1,67 @@
-package com.hireconnect.core.entity;public class Department {
+package com.hireconnect.core.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "departments")
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(exclude = {"company"})
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @NotBlank
+    @Size(max = 2000)
+    @Column(name = "description", nullable = false, length = 2000, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(nullable = false, name = "company_id")
+    private Company company;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Department(String name, String description, Company company) {
+        this.name = name;
+        this.description = description;
+        this.company = company;
+    }
+
+    public void setCompany(Company company) {
+        if (this.company != company) { // Evita loops ou sobrescritas desnecessárias
+            this.company = company;
+            if (company != null && !company.getDepartments().contains(this)) {
+                company.getDepartments().add(this); // Sincroniza o lado inverso
+            }
+        }
+    }
 }
--- src/main/java/com/hireconnect/core/repository/DepartmentRepository.java
+++ src/main/java/com/hireconnect/core/repository/DepartmentRepository.java
@@ -1,2 +1,10 @@
-package com.hireconnect.core.repository;public class DeparmentRepository {
+package com.hireconnect.core.repository;
+
+import com.hireconnect.core.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface DepartmentRepository extends JpaRepository<Department, UUID> {
+    boolean existsByNameAndCompanyId(String name, UUID companyId);
 }
