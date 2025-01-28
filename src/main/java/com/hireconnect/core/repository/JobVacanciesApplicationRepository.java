0a21178965c11dac069dedef145969db9a529e1d
Hericles Souza <hericles_003@hotmail.com>
2025-01-28 18:25:03 -0300

    feat(JobVacanciesApplication): create repository

--- src/main/java/com/hireconnect/core/repository/JobVacanciesApplicationRepository.java
+++ src/main/java/com/hireconnect/core/repository/JobVacanciesApplicationRepository.java
@@ -1,2 +1,11 @@
-package com.hireconnect.core.repository;public interface JobVacanciesApplicationRepository {
+package com.hireconnect.core.repository;
+
+import com.hireconnect.core.entity.JobVacanciesApplication;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface JobVacanciesApplicationRepository {
+    Optional<List<JobVacanciesApplication>> findByJobVacancyId(UUID uuid);
 }
